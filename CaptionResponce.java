b635a325aa3d034443814a8150f89c3fad8a1fd3
bilalbalouch <167913126+bilalbalouch@users.noreply.github.com>
2025-05-15 17:25:00 +0500

    Create CaptionResponce.java

---
+++ CaptionResponce.java
@@ -0,0 +1,13 @@
+package com.example.captionapp;
+
+public class CaptionResponse {
+    private String caption;
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+}
